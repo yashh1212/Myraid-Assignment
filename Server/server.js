f981106b61ce723632c194a280b02f24970e0217
yashh1212 <yashdhokane12@gmail.com>
2026-03-17 16:18:56 +0530

    update client  url

--- Server/server.js
+++ Server/server.js
@@ -18,7 +18,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'https://myraid-assignment-bice.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
